994ac8b7b1bf437619efc6de2b9c0cdba2eef9d0
Daria Ruzanova <dashochek-28@mail.ru>
2022-11-26 18:12:25 +0400

    Change camelCase

--- API_NASA/src/main/java/Post.java
+++ API_NASA/src/main/java/Post.java
@@ -5,8 +5,8 @@ public class Post {
     private final String data;
     private final String explanation;
     private final String hdurl;
-    private final String media_type;
-    private final String service_version;
+    private final String mediaType;
+    private final String serviceVersion;
     private final String title;
     private final String url;
 
@@ -14,16 +14,16 @@ public class Post {
                 @JsonProperty("date") String data,
                 @JsonProperty("explanation") String explanation,
                 @JsonProperty("hdurl") String hdurl,
-                @JsonProperty("media_type") String media_type,
-                @JsonProperty("service_version") String service_version,
+                @JsonProperty("media_type") String mediaType,
+                @JsonProperty("service_version") String serviceVersion,
                 @JsonProperty("title") String title,
                 @JsonProperty("url") String url) {
         this.copyright = copyright;
         this.data = data;
         this.explanation = explanation;
         this.hdurl = hdurl;
-        this.media_type = media_type;
-        this.service_version = service_version;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
@@ -40,12 +40,12 @@ public class Post {
         return hdurl;
     }
 
-    public String getMedia_type() {
-        return media_type;
+    public String getMediaType() {
+        return mediaType;
     }
 
-    public String getService_version() {
-        return service_version;
+    public String getServiceVersion() {
+        return serviceVersion;
     }
 
     public String getTitle() {
@@ -67,8 +67,8 @@ public class Post {
                 ", data='" + data + '\'' +
                 ", explanation='" + explanation + '\'' +
                 ", hdurl='" + hdurl + '\'' +
-                ", media_type='" + media_type + '\'' +
-                ", service_version='" + service_version + '\'' +
+                ", media_type='" + mediaType + '\'' +
+                ", service_version='" + serviceVersion + '\'' +
                 ", title='" + title + '\'' +
                 ", url='" + url + '\'' +
                 '}';
